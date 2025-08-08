6cfdb8bf9802ad3307b9450608dd7120f7f09f0f
chezLotti <charlottelhaw@gmail.com>
2025-08-08 17:50:51 +0200

    added the task as comment to "Order"

--- src/challenges/Order.groovy
+++ src/challenges/Order.groovy
@@ -1,7 +1,25 @@
 package challenges
 
 class Order {
+
   static void main(String[] args) {
+
+    /**
+     * Task:
+     *
+     * We intercepted one of Cipher's messages containing their next target. They encrypted their message using a
+     * repeating-key XOR cipher. However, they made a critical error—every message always starts with the header:
+     *
+     * ORDER:
+     *
+     * Can you help void decrypt the message and determine their next target?
+     * Here is the message we intercepted:
+     *
+     * 1c1c01041963730f31352a3a386e24356b3d32392b6f6b0d323c22243f6373
+     *
+     * 1a0d0c302d3b2b1a292a3a38282c2f222d2a112d282c31202d2d2e24352e60
+     *
+     */
 
     def message = "1c1c01041963730f31352a3a386e24356b3d32392b6f6b0d323c22243f63731a0d0c302d3b2b1a292a3a38282c2f222d2a112d282c31202d2d2e24352e60"
     List<Byte> messageAsBytes = message.decodeHex()
